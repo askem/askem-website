87a5bd47e35719de3f3f17bc6eb29da378a28b68
Yoni H <yonihemi@gmail.com>
2016-11-01 17:37:47 -0400

    Analytics

--- thanks.php
+++ thanks.php
@@ -234,5 +234,14 @@ if ($result === FALSE) {
     <script src="js/jquery.fancybox.js"></script>
     <script src="js/js-functions.js"></script>
 	<!-- <? echo $leadID ?> -->
+	<script src="https://askem.com/pixel.js?pub=Askem&campaign=home_lead" async="async" defer="defer"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-34753664-1', 'auto');
+  ga('send', 'pageview');
+</script>
 </body>
 </html>
