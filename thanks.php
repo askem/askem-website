64df39f4379a74c65c9fe6843fcbb946414e944b
Yoni H <yonihemi@gmail.com>
2016-11-21 23:13:48 +0800

    Update tracking - GA, FB, LinkedIn

--- thanks.php
+++ thanks.php
@@ -246,8 +246,37 @@ if ($result === FALSE) {
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-34753664-1', 'auto');
+  ga('create', 'UA-87736891-1', 'auto');
   ga('send', 'pageview');
 </script>
+
+<script>
+	_linkedin_data_partner_id = "21993";
+</script>
+<script>
+(function(){var s = document.getElementsByTagName("script")[0];
+var b = document.createElement("script");
+b.type = "text/javascript";b.async = true;
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+s.parentNode.insertBefore(b, s);})();
+</script>
+
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+ fbq('init', '1848903508664772'); 
+fbq('track', 'PageView');
+</script>
+<noscript>
+ <img height="1" width="1" 
+src="https://www.facebook.com/tr?id=1848903508664772&ev=PageView&noscript=1"/>
+</noscript>
+
 </body>
 </html>
